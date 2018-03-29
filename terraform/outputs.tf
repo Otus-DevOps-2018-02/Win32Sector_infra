bd3f0f50096bc74fc18bdbef408b1290df14e3f1
Pavel Kozlov <sovvvest@yandex.ru>
2018-03-30 00:55:56 +0500

    public_keys.pub moved to ~/.ssh/ and fixed ways to this file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "app_external_ip_reddit-app" {
-  value = "${google_compute_instance.app.0.network_interface.0.access_config.0.assigned_nat_ip}"
-}
-output "app_external_ip_reddit_app2" {
-  value = "${google_compute_instance.app.1.network_interface.0.access_config.0.assigned_nat_ip}"
-}
-output "load_balancer_ip" {
-  value = "${google_compute_forwarding_rule.default.ip_address}"
-}
+#output "app_external_ip_reddit-app" {
+#  value = "${google_compute_instance.app.0.network_interface.0.access_config.0.assigned_nat_ip}"
+#}
+#output "app_external_ip_reddit_app2" {
+#  value = "${google_compute_instance.app.1.network_interface.0.access_config.0.assigned_nat_ip}"
+#}
+#output "load_balancer_ip" {
+#  value = "${google_compute_forwarding_rule.default.ip_address}"
+#}
